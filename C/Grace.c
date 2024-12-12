4f1991444e9087850ad56a3e9a78f683363b0a7f
William Millette <wmillett@c4c6c5.42berlin.de>
2024-12-12 19:12:43 +0100

    started Grace

--- C/Grace.c
+++ C/Grace.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
 
-/*
-    One comment is very Graceful
-*/
+#define SOURCE "#define SOURCE %c%s%c\\n#define FILENAME %cGrace_kid.c%c\\n#define PRINT_FILE(x) { FILE* f = fopen(x, %cw%c); fprintf(f, SOURCE, 34, SOURCE, 34, 34, FILENAME, 34); fclose(f); } /* Grace quine */"
+#define FILENAME "Grace_kid.c"
+#define PRINT_FILE(x) { FILE* f = fopen(x, "w"); fprintf(f, SOURCE, 34, SOURCE, 34, 34, FILENAME, 34); fclose(f); }
 
-int Grace(){
-
-    return 0;
-}
+PRINT_FILE(FILENAME)
