3516995664b7098d66de22406b11c6e726116658
wmillett <williammillette98@gmail.com>
2024-12-26 18:37:01 +0100

    finished Colleen.s and fixed messaging in makefile, note: change back _main and _printf for Linux

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,6 +1,6 @@
 ; Comment outside
 section .data
-    format db ";Comment outside%1$csection .data%1$c    format db %2$c%3$s%2$c,10,34,format%1$c%1$csection .text%1$c    extern printf%1$c    global main%1$%1$cmain:%1$c    push rbp%1$c   mov rbp, rsp%1$c    ; Comment in main%1$c   sub rsp, 16%1$c    lea rdi, [rel format]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel format]%1$c    xor rax, rax%1$c    call _printf%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c"
+    format db "; Comment outside%1$csection .data%1$c    format db %2$c%3$s%2$c%1$c%1$csection .text%1$c    extern _printf%1$c    global _main%1$c%1$c_main:%1$c    push rbp%1$c    mov rbp, rsp%1$c    ; Comment in main%1$c    sub rsp, 16%1$c    lea rdi, [rel format]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel format]%1$c    xor rax, rax%1$c    call _printf%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c"
 
 section .text
     extern _printf
