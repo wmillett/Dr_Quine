a7f3b4cc5bd6e0deaf2a7710e46e91d71f02d360
William Millette <wmillett@c3a8c1.42berlin.de>
2025-01-02 20:22:07 +0100

    trying to fix for linux, still need to solve printf issue

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,23 +1,25 @@
 ; Comment outside
 section .data
-    format db "; Comment outside%1$csection .data%1$c    format db %2$c%3$s%2$c%1$c%1$csection .text%1$c    extern _printf%1$c    global _main%1$c%1$c_main:%1$c    push rbp%1$c    mov rbp, rsp%1$c    ; Comment in main%1$c    sub rsp, 16%1$c    lea rdi, [rel format]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel format]%1$c    xor rax, rax%1$c    call _printf%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c"
+    format db "; Comment outside%1$csection .data%1$c    format db %2$c%3$s%2$c%1$c%1$csection .text%1$c    extern printf%1$c    global _start%1$c%1$c_start:%1$c    push rbp%1$c    mov rbp, rsp%1$c    ; Comment in _start%1$c    sub rsp, 16%1$c    lea rdi, [rel format]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel format]%1$c    xor rax, rax%1$c    call printf%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c"
 
 section .text
-    extern _printf
-    global _main
+    extern printf
+    global _start
 
-_main:
+_start:
     push rbp
     mov rbp, rsp
-    ; Comment in main
+    ; Comment in _start
     sub rsp, 16
     lea rdi, [rel format]
     mov rsi, 10
     mov rdx, 34
     lea rcx, [rel format]
     xor rax, rax
-    call _printf
+    call printf
     mov rsp, rbp
     pop rbp
     xor rax, rax
-    ret
+    mov rax, 60 ;delimiter (need to change format string from here on out)
+    xor rdi, rdi
+    syscall
