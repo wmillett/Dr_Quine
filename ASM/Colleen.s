7c5c84666b749bb61729d76942c61597b2b87784
William Millette <wmillett@c3a4c6.42berlin.de>
2025-01-03 18:56:10 +0100

    Colleen asm works on linux

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,9 +1,11 @@
 ; Comment outside
 section .data
-    format db "; Comment outside%1$csection .data%1$c    format db %2$c%3$s%2$c%1$c%1$csection .text%1$c    extern printf%1$c    global main%1$c%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    and rsp, -16%1$c    ; Comment in main%1$c    sub rsp, 16%1$c    lea rdi, [rel format]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel format]%1$c    xor rax, rax%1$c    call printf%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    mov rax, 60%1$c    xor rdi, rdi%1$c    syscall%1$c"
+    format db "; Comment outside%1$csection .data%1$c    format db %2$c%3$s%2$c%1$c%1$csection .text%1$c    extern printf%1$c    extern stdout%1$c    extern fflush%1$c    global main%1$c%1$cleave:%1$c    mov rax, 60%1$c    xor rdi, rdi%1$c    syscall%1$c%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    and rsp, -16%1$c    ; Comment in main%1$c    sub rsp, 16%1$c    mov rdi, format%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, format%1$c    xor rax, rax%1$c    call printf%1$c    mov rdi, [rel stdout]%1$c    call fflush%1$c    call leave%1$c"
 
 section .text
     extern printf
+    extern stdout
+    extern fflush
     global main
 
 leave:
@@ -17,10 +19,12 @@ main:
     and rsp, -16
     ; Comment in main
     sub rsp, 16
-    lea rdi, [rel format]
+    mov rdi, format
     mov rsi, 10
     mov rdx, 34
-    lea rcx, [rel format]
+    mov rcx, format
     xor rax, rax
     call printf
+    mov rdi, [rel stdout]
+    call fflush
     call leave
